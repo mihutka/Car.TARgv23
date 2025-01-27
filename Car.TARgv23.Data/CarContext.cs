eed398bdaaa4650ea51ac5f73ab90a5937251cb7
misab <misabajandinov@gmail.com>
2025-01-27 15:52:34 +0200

    add CarContext

---
+++ Car.TARgv23.Data/CarContext.cs
@@ -0,0 +1,18 @@
+﻿using Car.TARgv23.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.TARgv23.Data
+{
+     public class CarContext : DbContext
+     {
+        public CarContext(DbContextOptions<CarContext> options)
+            : base(options) { }
+
+        public DbSet<CarDomain> Cars { get; set; }
+    }
+}
