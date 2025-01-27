beb3c12c1b7aa24bec8abcbc36690b1553fb4f82
misab <misabajandinov@gmail.com>
2025-01-27 15:40:27 +0200

    add CarDto, Domain

---
+++ Car.TARgv23.Core/Dto/CarDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.TARgv23.Core.Dto
+{
+    public class CarDto
+    {
+        public Guid Id { get; set; }
+
+        public string? Brand { get; set; }
+
+        public string? Model { get; set; }
+
+        public string? Number { get; set; }
+
+        public int? Mileage { get; set; }
+
+        public bool? TechnicalInspection { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
