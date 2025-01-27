b6fc669267ae820253b74dc92a57867079681a95
misab <misabajandinov@gmail.com>
2025-01-27 15:38:47 +0200

    Добавьте файлы проекта.

---
+++ Car.TARgv23/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Car.TARgv23.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
